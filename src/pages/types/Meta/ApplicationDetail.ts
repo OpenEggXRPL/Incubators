59aae4a50657ec7ed50be36d3a74d41ae7b32b6a
OpenEgg <114755259+OpenEggXRPL@users.noreply.github.com>
2023-06-30 13:39:17 +0100

    Create ApplicationDetail.ts

---
+++ src/pages/types/Meta/ApplicationDetail.ts
@@ -0,0 +1,12 @@
+export interface ApplicationDetails {
+  quota?: Record<string, unknown>;
+  application: {
+    uuidv4: string;
+    name: string;
+    webhookurl?: string;
+    disabled?: number;
+  };
+  call?: {
+    uuidv4: string;
+  };
+}
